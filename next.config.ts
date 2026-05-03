728117fc152200bf295d0c1bf0c1dcb95976e721
Palett IQ <palettiq8@gmail.com>
2026-05-03 16:32:47 +0600

    Refactor: Update X-Frame-Options header value from DENY to SAMEORIGIN

--- next.config.ts
+++ next.config.ts
@@ -26,7 +26,7 @@ const nextConfig: NextConfig = {
           },
           {
             key: "X-Frame-Options",
-            value: "DENY",
+            value: "SAMEORIGIN",
           },
           {
             key: "X-XSS-Protection",
